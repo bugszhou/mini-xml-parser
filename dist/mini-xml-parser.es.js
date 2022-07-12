3d0105f4788bf1f6c27802c2be3772405955c023
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-07-12 16:56:39 +0800

    feat: Upgrade dep

--- dist/mini-xml-parser.es.js
+++ dist/mini-xml-parser.es.js
@@ -2235,7 +2235,7 @@ class Tokenizer {
                 break;
             }
             default: {
-                token.tagName += String.fromCodePoint(isAsciiUpper(cp) ? toAsciiLower(cp) : cp);
+                token.tagName += String.fromCodePoint(cp);
             }
         }
     }
@@ -8091,126 +8091,9 @@ function endTagInForeignContent(p, token) {
 
 });
 
-var _escape = createCommonjsModule(function (module, exports) {
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.escapeText = exports.escapeAttribute = exports.escapeUTF8 = exports.escape = exports.encodeXML = exports.getCodePoint = exports.xmlReplacer = void 0;
-exports.xmlReplacer = /["&'<>$\x80-\uFFFF]/g;
-var xmlCodeMap = new Map([
-    [34, "&quot;"],
-    [38, "&amp;"],
-    [39, "&apos;"],
-    [60, "&lt;"],
-    [62, "&gt;"],
-]);
-// For compatibility with node < 4, we wrap `codePointAt`
-exports.getCodePoint = 
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-String.prototype.codePointAt != null
-    ? function (str, index) { return str.codePointAt(index); }
-    : // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-        function (c, index) {
-            return (c.charCodeAt(index) & 0xfc00) === 0xd800
-                ? (c.charCodeAt(index) - 0xd800) * 0x400 +
-                    c.charCodeAt(index + 1) -
-                    0xdc00 +
-                    0x10000
-                : c.charCodeAt(index);
-        };
-/**
- * Encodes all non-ASCII characters, as well as characters not valid in XML
- * documents using XML entities.
- *
- * If a character has no equivalent entity, a
- * numeric hexadecimal reference (eg. `&#xfc;`) will be used.
- */
-function encodeXML(str) {
-    var ret = "";
-    var lastIdx = 0;
-    var match;
-    while ((match = exports.xmlReplacer.exec(str)) !== null) {
-        var i = match.index;
-        var char = str.charCodeAt(i);
-        var next = xmlCodeMap.get(char);
-        if (next !== undefined) {
-            ret += str.substring(lastIdx, i) + next;
-            lastIdx = i + 1;
-        }
-        else {
-            ret += "".concat(str.substring(lastIdx, i), "&#x").concat((0, exports.getCodePoint)(str, i).toString(16), ";");
-            // Increase by 1 if we have a surrogate pair
-            lastIdx = exports.xmlReplacer.lastIndex += Number((char & 0xfc00) === 0xd800);
-        }
-    }
-    return ret + str.substr(lastIdx);
-}
-exports.encodeXML = encodeXML;
-/**
- * Encodes all non-ASCII characters, as well as characters not valid in XML
- * documents using numeric hexadecimal reference (eg. `&#xfc;`).
- *
- * Have a look at `escapeUTF8` if you want a more concise output at the expense
- * of reduced transportability.
- *
- * @param data String to escape.
- */
-exports.escape = encodeXML;
-function getEscaper(regex, map) {
-    return function escape(data) {
-        var match;
-        var lastIdx = 0;
-        var result = "";
-        while ((match = regex.exec(data))) {
-            if (lastIdx !== match.index) {
-                result += data.substring(lastIdx, match.index);
-            }
-            // We know that this chararcter will be in the map.
-            result += map.get(match[0].charCodeAt(0));
-            // Every match will be of length 1
-            lastIdx = match.index + 1;
-        }
-        return result + data.substring(lastIdx);
-    };
-}
-/**
- * Encodes all characters not valid in XML documents using XML entities.
- *
- * Note that the output will be character-set dependent.
- *
- * @param data String to escape.
- */
-exports.escapeUTF8 = getEscaper(/[&<>'"]/g, xmlCodeMap);
-/**
- * Encodes all characters that have to be escaped in HTML attributes,
- * following {@link https://html.spec.whatwg.org/multipage/parsing.html#escapingString}.
- *
- * @param data String to escape.
- */
-exports.escapeAttribute = getEscaper(/["&\u00A0]/g, new Map([
-    [34, "&quot;"],
-    [38, "&amp;"],
-    [160, "&nbsp;"],
-]));
-/**
- * Encodes all characters that have to be escaped in HTML text,
- * following {@link https://html.spec.whatwg.org/multipage/parsing.html#escapingString}.
- *
- * @param data String to escape.
- */
-exports.escapeText = getEscaper(/[&<>\u00A0]/g, new Map([
-    [38, "&amp;"],
-    [60, "&lt;"],
-    [62, "&gt;"],
-    [160, "&nbsp;"],
-]));
-
-});
-
-var escape_js_1 = _escape;
-
 var serializer = createCommonjsModule(function (module, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serializeOuter = exports.serialize = void 0;
-
 
 
 // Sets
@@ -8239,7 +8122,10 @@ function isVoidElement(node, options) {
         options.treeAdapter.getNamespaceURI(node) === html.NS.HTML &&
         VOID_ELEMENTS.has(options.treeAdapter.getTagName(node)));
 }
-const defaultOpts = { treeAdapter: _default.defaultTreeAdapter, scriptingEnabled: true };
+const defaultOpts = {
+    treeAdapter: _default.defaultTreeAdapter,
+    scriptingEnabled: true,
+};
 /**
  * Serializes an AST node to an HTML string.
  *
@@ -8265,7 +8151,7 @@ const defaultOpts = { treeAdapter: _default.defaultTreeAdapter, scriptingEnabled
 function serialize(node, options) {
     const opts = Object.assign(Object.assign({}, defaultOpts), options);
     if (isVoidElement(node, opts)) {
-        return '';
+        return "";
     }
     return serializeChildNodes(node, opts);
 }
@@ -8295,7 +8181,7 @@ function serializeOuter(node, options) {
 }
 exports.serializeOuter = serializeOuter;
 function serializeChildNodes(parentNode, options) {
-    let html$1 = '';
+    let html$1 = "";
     // Get container of the child nodes
     const container = options.treeAdapter.isElementNode(parentNode) &&
         options.treeAdapter.getTagName(parentNode) === html.TAG_NAMES.TEMPLATE &&
@@ -8324,16 +8210,18 @@ function serializeNode(node, options) {
         return serializeDocumentTypeNode(node, options);
     }
     // Return an empty string for unknown nodes
-    return '';
+    return "";
 }
 function serializeElement(node, options) {
     const tn = options.treeAdapter.getTagName(node);
-    return `<${tn}${serializeAttributes(node, options)}>${isVoidElement(node, options) ? '' : `${serializeChildNodes(node, options)}</${tn}>`}`;
+    return `<${tn}${serializeAttributes(node, options)}>${isVoidElement(node, options)
+        ? ""
+        : `${serializeChildNodes(node, options)}</${tn}>`}`;
 }
 function serializeAttributes(node, { treeAdapter }) {
-    let html$1 = '';
+    let html$1 = "";
     for (const attr of treeAdapter.getAttrList(node)) {
-        html$1 += ' ';
+        html$1 += " ";
         if (!attr.namespace) {
             html$1 += attr.name;
         }
@@ -8344,8 +8232,8 @@ function serializeAttributes(node, { treeAdapter }) {
                     break;
                 }
                 case html.NS.XMLNS: {
-                    if (attr.name !== 'xmlns') {
-                        html$1 += 'xmlns:';
+                    if (attr.name !== "xmlns") {
+                        html$1 += "xmlns:";
                     }
                     html$1 += attr.name;
                     break;
@@ -8358,20 +8246,14 @@ function serializeAttributes(node, { treeAdapter }) {
                     html$1 += `${attr.prefix}:${attr.name}`;
                 }
             }
-        html$1 += `="${(0, escape_js_1.escapeAttribute)(attr.value)}"`;
+        html$1 += `="${attr.value}"`;
     }
     return html$1;
 }
 function serializeTextNode(node, options) {
     const { treeAdapter } = options;
     const content = treeAdapter.getTextNodeContent(node);
-    const parent = treeAdapter.getParentNode(node);
-    const parentTn = parent && treeAdapter.isElementNode(parent) && treeAdapter.getTagName(parent);
-    return parentTn &&
-        treeAdapter.getNamespaceURI(parent) === html.NS.HTML &&
-        (0, html.hasUnescapedText)(parentTn, options.scriptingEnabled)
-        ? content
-        : (0, escape_js_1.escapeText)(content);
+    return content;
 }
 function serializeCommentNode(node, { treeAdapter }) {
     return `<!--${treeAdapter.getCommentNodeContent(node)}-->`;
